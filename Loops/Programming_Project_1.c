1502fcce142772a3dd14b7a86452aece3eb98162
kizitoomot <kizitoomoit9@gmail.com>
2025-04-21 10:51:33 +0300

    This idea came to me while I was on Loops

---
+++ Loops/Programming_Project_1.c
@@ -0,0 +1,48 @@
+/* Question:  Write a programm that finds the largest in a series of number by
+ * the user. The program must prompt the user to enter the numbers one by one.
+ * When the user enters 0 or a negative number, the program must display the
+ * laragest nonnegative number entered: */
+
+/* Enter a number: 60
+ Enter a number: 38.3
+ Enter a number: 4.89
+ Enter a number: 100.62
+ Enter a number: 75.2295
+ Enter a number: 0
+
+  The largest number entered was 100.62 */
+
+/* Solution */
+
+#include <stdio.h>
+
+int main(void) {
+  float number, largest_number = 0;
+
+  printf("Enter a number ( Enter 0 to quit ): ");
+  scanf("%f", &number);
+
+  /* Check the entered number if it is not 0 */
+  while (number != 0) {
+    /* Enter the loop if the number is no 0 */
+
+    /* Check if the current entered number is larger than than the currently
+     * largest number */
+    if (number > largest_number) {
+
+      /* if the current entered number is larger than largest the assing it to
+       * the largest */
+      largest_number = number;
+    }
+
+    /* Prompt the user again to enter a number, it is hard to avoid this in a
+     * while loop */
+    printf("Enter a number ( Enter 0 to quit ): ");
+    scanf("%f", &number);
+  }
+
+  /* If the user enters 0 quit the loop and print the current largest number */
+  printf("The largest number entered was: %f\n", largest_number);
+
+  return 0;
+}
